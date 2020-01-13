97d8061310d8c1bf2c989c98c4fc25b3bc89f85d
WincerChan <WincerChan@gmail.com>
2020-01-13 06:06:00 +0100

    :wrench: Remove database, use dets

---
+++ lib/douban_show/persist.ex
@@ -0,0 +1,48 @@
+defmodule DoubanShow.Persist do
+  use GenServer
+
+  alias :mnesia, as: Mnesia
+
+  @table :douban
+  @attributes [
+    :id, :category, :url,
+    :date, :tags, :title,
+    :cover, :rating, :comment
+  ]
+  def init_mnesia do
+    Mnesia.create_schema([node()])
+    Mnesia.start()
+    Mnesia.create_table(@table, [attributes: @attributes, disc_only_copies: [node()]])
+    Mnesia.wait_for_tables([@table], 5000)
+  end
+
+  def do_save(item) do
+    id = :crypto.hash(:md5, "#{item |> Enum.at(2)}#{item |> Enum.at(4)}" |> IO.inspect)
+    [@table | [id | item]]
+    |> List.to_tuple
+    |> Mnesia.write
+  end
+
+  def save_record(item) do
+    Mnesia.transaction(
+      fn ->
+      do_save(item)
+      end
+    )
+  end
+
+  def start_link(_) do
+    state = 1
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(state) do
+    init_mnesia()
+    {:ok, state}
+  end
+
+  def close do
+    Mnesia.stop()
+  end
+
+end
