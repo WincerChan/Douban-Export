3a4eadff7e72516f641225576bd0cd8947afa49a
WincerChan <WincerChan@gmail.com>
2020-01-25 16:31:26 +0800

    :wrench: Change mnesia date format

--- lib/show.ex
+++ lib/show.ex
@@ -5,23 +5,33 @@ defmodule Show do
     Mnesia.start()
   end
 
+  def this_year do
+    Date.utc_today
+    |> Date.add(-Date.day_of_year(Date.utc_today))
+    |> Date.to_iso8601
+  end
+
   def get_this_year do
     Mnesia.dirty_select(
       :douban,
       [
         {{:douban, :"$1", :"$2", :"$3", :"$4", :"$5", :"$6", :"$7", :"$8", :"$9"},
-         [{:>, :"$4", "2020-00-00"}], [:"$$"]}
+         [{:>, :"$4", this_year()}],
+         [:"$$"]}
       ]
     )
     |> Enum.sort_by(&Enum.at(&1, 3))
   end
 
-  def draw do
+  def draw(item) do
+    [_, category, url, date, _, title, cover, _, _] = item
+    IO.puts("{% figure '#{cover}' '#{title}' '#{url}' '#{date <> if category == "book", do: " 读过", else: " 看过"}' %}")
+  end
+
+  def start do
     init()
     get_this_year()
-    |> Stream.map(
-      &IO.puts("{% figure '#{&1 |> Enum.at(6)}' '#{&1 |> Enum.at(5)}' '#{&1 |> Enum.at(2)}' '#{Enum.at(&1, 3) <> if Enum.at(&1, 1) == "book", do: " 读过", else: " 看过"}' %}")
-    )
+    |> Stream.map(&draw/1)
     |> Enum.to_list
   end
 end
