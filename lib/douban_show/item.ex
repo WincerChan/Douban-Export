fe6beb8eb979af2d19624d1627ac71fb96474369
WincerChan <WincerChan@gmail.com>
2020-01-25 16:30:38 +0800

    :wrench: Add inspect item before sha3_256

--- lib/douban_show/item.ex
+++ lib/douban_show/item.ex
@@ -1,13 +1,17 @@
 defmodule DoubanItem do
-    def new, do: []
+  def new, do: []
 
-    def put(inst, value) do
-        [value | inst]
-    end
+  def put(inst, value) do
+    [value | inst]
+  end
 
-    def make_id(inst) do
-        with sum <- :crypto.hash(:sha3_256, inst) do
-            [sum | inst]
-        end
+  defp new_id(item) do
+    :crypto.hash(:sha3_256, inspect(item))
+  end
+
+  def make_id(inst) do
+    with sum <- new_id(inst) do
+      [sum | inst]
     end
+  end
 end
