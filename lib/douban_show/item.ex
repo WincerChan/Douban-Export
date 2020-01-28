db76b8ca313624c4b5504c463f7295e7c2bb100e
WincerChan <WincerChan@gmail.com>
2020-01-28 06:06:00 +0100

    :wrench: Refactor douban item use GenServer

--- lib/douban_show/item.ex
+++ lib/douban_show/item.ex
@@ -1,17 +1,42 @@
 defmodule DoubanItem do
-  def new, do: []
+  use GenServer
 
-  def put(inst, value) do
-    [value | inst]
+  def new do
+    GenServer.start(__MODULE__, nil)
   end
 
-  defp new_id(item) do
-    :crypto.hash(:sha3_256, inspect(item))
+  def init(_) do
+    {:ok, []}
   end
 
-  def make_id(inst) do
-    with sum <- new_id(inst) do
-      [sum | inst]
+  def put(pid, value) do
+    GenServer.cast(pid, {:put, value})
+  end
+
+  def get(pid) do
+    GenServer.call(pid, {:get})
+  end
+
+  def identify(pid) do
+    GenServer.cast(pid, {:identify})
+  end
+
+  def handle_cast({:put, value}, inst) do
+    {:noreply, [value | inst]}
+  end
+
+  def handle_call({:get}, _, inst) do
+    {:reply, inst, inst}
+  end
+
+  def handle_cast({:identify}, inst) do
+    case String.printable?(inst |> hd) do
+      true -> {:noreply, [id(inst) | inst]}
+      false -> {:noreply, inst}  # already indentified
     end
+  end
+
+  defp id(item) do
+    :crypto.hash(:sha3_256, inspect(item))
   end
 end
